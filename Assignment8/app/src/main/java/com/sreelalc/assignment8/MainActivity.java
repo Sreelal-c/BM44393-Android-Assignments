86c897d94635dfb31a1d11f7169fd62a1f23f866
Sreelal C <sreelal.ub@gmail.com>
2017-07-18 21:19:49 +0530

    Assignment  - Menu

---
+++ Assignment8/app/src/main/java/com/sreelalc/assignment8/MainActivity.java
@@ -0,0 +1,41 @@
+package com.sreelalc.assignment8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this, "Settings",
+                        Toast.LENGTH_LONG).show();
+
+                break;
+            case R.id.help:
+                Toast.makeText(this, "Help", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
