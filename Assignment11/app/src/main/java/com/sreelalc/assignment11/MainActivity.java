8d0975abb2516af9f40014a213c061d62b656383
Sreelal C <sreelal.ub@gmail.com>
2017-07-19 12:25:32 +0530

    Assignment  11 - Spinner

---
+++ Assignment11/app/src/main/java/com/sreelalc/assignment11/MainActivity.java
@@ -0,0 +1,36 @@
+package com.sreelalc.assignment11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.courses);
+        List<String> categories = new ArrayList<String>();
+        categories.add("PHP");
+        categories.add("Java");
+        categories.add("Android");
+        categories.add("ASP.NET");
+        categories.add("Python");
+        categories.add("HTML");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+    }
+
+
+}
